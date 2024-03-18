00d977cfe72669f29f4b0b620ef40e0f4655d820
Maaaaaaaad <wowa-iff@yandex.ru>
2024-03-18 16:12:00 +0300

    Lesion_2

---
+++ src/Gendiff.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Differ\src\genDiff;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+
+
+function difference ($file1, $file2, $format)
+{
+
+}
